a0148760b56de91fdf7063b07a6d9c83ad3a5bc0
ethan7wce <114141551+ethan7wce@users.noreply.github.com>
2022-11-30 14:13:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FacebookLoginComponent from './facebooklogin.component';
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Facebook Login Tutorial in React App</h1> 
+       
+       <FacebookLoginComponent />
+
+    </div>
+  );
+}
+
+export default App;
+
